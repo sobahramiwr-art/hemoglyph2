caebbf26d04fe673e8e61af1bea474c209a2ff0a
Sobhan Bahrami Samani <sobahramiwr@gmail.com>
2026-07-16 22:54:59 +0330

    Update app.py

--- app.py
+++ app.py
@@ -679,7 +679,7 @@ draw();
             qr_bytes = f.read()
         qr_b64 = base64.b64encode(qr_bytes).decode()
         st.markdown(
-    f'<img src="data:image/png;base64,{qr_b64}" style="width: 80px; height: auto; margin-top: 10px;">',
+    f'<img src="data:image/png;base64,{qr_b64}" style="width: 140px; height: auto; margin-top: 10px;">',
     unsafe_allow_html=True
 )
 
